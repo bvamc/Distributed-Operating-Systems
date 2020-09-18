0f0a5967d61fd49818b4c71761a718b261ab4c80
bvamc <vamsidharreddybad@gmail.com>
2020-09-18 15:33:36 -0400

    Working v2.0

--- Proj1/proj1.fsx
+++ Proj1/proj1.fsx
@@ -16,28 +16,28 @@ let len = fsi.CommandLineArgs.[2] |> int
 
 
 let sumOfSquares numberList = numberList |> List.sumBy (fun x -> x * x)
-let isPerfectSquare (number:int) = sqrt (float number) |> fun n -> (n = floor(n))
+let isPerfectSquare (number:int64) = sqrt (float number) |> fun n -> (n = floor(n))
 
 type EchoServer(name) =
     inherit Actor()
     
     override x.OnReceive message =
        match message with
-        | :? int as n -> 
-            sumOfSquares [n .. n+len-1] |> fun sq -> if(isPerfectSquare sq) then printfn "%i" n
+        | :? int64 as n -> 
+            sumOfSquares [n .. (n + int64(len-1))] |> fun sq -> if(isPerfectSquare sq) then printfn "%i" n
         | _ -> failwith "unknown message"
 
 let echoServers =
-    [ 1 .. maxNum ] |> List.map (fun id ->
+    [ 1 .. 8 ] |> List.map (fun id ->
         let properties = [| string (id) :> obj |]
         system.ActorOf(Props(typedefof<EchoServer>, properties)))
 
 let actorRef = echoServers
 // printfn "%A" actorRef
-let mutable m = 1
-for ref in actorRef do
-    ref <! m
-    m <- m+1
+let mutable cur = 0
+for num in [1 .. maxNum] do
+    cur <- (num%8)
+    actorRef.Item(cur) <! int64(num)
 
 Console.Read() |> ignore
 system.Terminate()
