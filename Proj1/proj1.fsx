12230b35090bae696c17403fe9a9830fdc4fbd7b
bvamc <vamsidharreddybad@gmail.com>
2020-09-21 19:56:21 -0400

    Remote test 1

--- Proj1/proj1.fsx
+++ Proj1/proj1.fsx
@@ -3,29 +3,23 @@
 
 open System
 open Akka.Actor
-open Akka.Configuration
 open Akka.FSharp
-
-// if (fsi.CommandLineArgs.Length<>2) 
-//     then printfn "Invalid number of arguments"
-// else 
 
 let system = ActorSystem.Create("FSharp")
 let maxNum = fsi.CommandLineArgs.[1] |> int
 let len = fsi.CommandLineArgs.[2] |> int
 
-let numberOfActors = 2000
+let numberOfActors = Environment.ProcessorCount
 let workUnit = 100
 
-let sumOfSquares numberList = numberList |> List.sumBy (fun x -> x * x)
 let square  = fun (x:uint64)->x*x
+let sumOfSquares numberList = numberList |> List.sumBy (fun x -> (square x))
 let isPerfectSquare (number:uint64) = (square (uint64(sqrt (double number)))) = number
 
 let mutable count = numberOfActors 
 
 type PrintServer(name) =
     inherit Actor()
-    
     override x.OnReceive message =
        match message with
         | :? uint64 as n -> 
@@ -46,7 +40,7 @@ type CountServer(name) =
             count <- count - 1
         | _ -> failwith "unknown message"
 
-let countProperties = [| "printer" :> obj |]
+let countProperties = [| "counter" :> obj |]
 let countActor = system.ActorOf(Props(typedefof<CountServer>, countProperties))
 
 type EchoServer(name) =
@@ -59,17 +53,13 @@ type EchoServer(name) =
             let rangeEnd = min (rangeStart + uint64(workUnit-1)) (uint64(maxNum))
             for k in [rangeStart .. rangeEnd] do
                 sumOfSquares [k .. (k + uint64(len-1))] |> fun sq -> if(isPerfectSquare sq) then printActor <! k
-                (*let sum = [k .. (k + uint64(len-1))] |> List.sumBy (fun x -> x * x)
-                let sq = float sum
-                if sq = sqrt sq then 
-                    printActor <! k*)
 
             if rangeStart + uint64(workUnit * numberOfActors) - uint64(1) > uint64(maxNum) then
                 countActor <! 1
         | _ -> failwith "unknown message"
 
 
-
+//Create list of actors
 let echoServers =
     [ 1 .. numberOfActors ] |> List.map (fun id ->
         let properties = [| string (id) :> obj |]
@@ -78,10 +68,10 @@ let echoServers =
 
 let actorRef = echoServers
 let mutable cur = 0
-let temp = maxNum/workUnit
+let rangeCount = maxNum/workUnit
 
 
-for num in [0 .. temp] do
+for num in [0 .. rangeCount] do
     cur <- (num%numberOfActors)
     if num < numberOfActors then
         countActor <! "1"
--- Proj1/references.fsx
+++ Proj1/references.fsx
@@ -1,2 +1,3 @@
 #r @"bin/Debug/netcoreapp3.1/Akka.FSharp.dll"
 #r @"bin/Debug/netcoreapp3.1/Akka.dll"
+#r @"bin/Debug/netcoreapp3.1/Akka.Remote.dll"
